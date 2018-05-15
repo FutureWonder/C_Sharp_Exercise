e299a45d6be9a7748bfb47640f021eb0444dabf5
Chase <crussell@dvtrading.co>
2018-05-15 16:35:32 -0400

    Cleaning up exercise for VS 2017 and bitbucket migration

--- CandidateExercise/ConfigurationExercise/Properties/AssemblyInfo.cs
+++ CandidateExercise/ConfigurationExercise/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Configuration_Exercise")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
